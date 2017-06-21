9d4cb14d8155cb9130ab0253331848f0e9f698ec
Alessandro Roux <castellanese@gmail.com>
2017-06-20 23:16:13 -0400

    Set up Problem 3.

---
+++ Problem3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problem3
+//
+//  Created by Alessandro Roux on 6/20/17.
+//  Copyright © 2017 castellanese. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
